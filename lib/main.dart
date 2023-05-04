e2e394b3c5b591c648f63c8e20f0c6dbc7378322
Arthur <arthurbufon2052@gmail.com>
2023-05-03 21:39:59 -0300

    alterado tema e adicionado appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,25 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget
-{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context)
-  {
-    return const MaterialApp(
-      home: Text('Hello World')
-    );
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.purple,
+          ),
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('WordPair Generator')),
+            body: Center(
+              child: Text('Hello World!'),
+            )
+        ));
   }
 }

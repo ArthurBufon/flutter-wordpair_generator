7f9cb87bb8ed5ab2d4ed3ca12cb551fb1bbefd42
Arthur <arthurbufon2052@gmail.com>
2023-05-03 21:59:55 -0300

    adicionado listview, build e divider

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
         theme: ThemeData(
           appBarTheme: AppBarTheme(
@@ -21,20 +20,43 @@ class MyApp extends StatelessWidget {
 }
 
 // ignore: use_key_in_widget_constructors
-class RandomWords extends StatefulWidget
-{
+class RandomWords extends StatefulWidget {
   @override
   RandomWordsState createState() => RandomWordsState();
 }
 
-class RandomWordsState extends State<RandomWords>
-{
-  @override
-  Widget build(BuildContext context)
-  {
-    return Scaffold(
-      appBar: AppBar(title: Text('WordPair Generator')),
-      body: Text('Hello'),
+class RandomWordsState extends State<RandomWords> {
+  final _randomWordPairs = <WordPair>[];
+
+  Widget _buildList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, item)
+      {
+        if(item.isOdd) return Divider();
+
+        final index = item ~/ 2;
+
+        if(index >= _randomWordPairs.length)
+        {
+          _randomWordPairs.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_randomWordPairs[index]);
+      },
     );
   }
-} 
+
+  Widget _buildRow(WordPair pair)
+  {
+    return ListTile(title: Text('Hello'),);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('WordPair Generator')),
+      body: _buildList(),
+    );
+  }
+}
